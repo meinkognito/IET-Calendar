4941501f44447abbd97b9a25c7e9894e9f67858b
Denis Beloshitskiy <beloshitskiy.denis@gmail.com>
2022-03-17 17:32:11 +0300

    Add ViewControllers group

--- IET Calendar/ViewContollers/ViewController.swift
+++ IET Calendar/ViewContollers/ViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  IET Calendar
-//
-//  Created by Denis Beloshitskiy on 2/10/22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    
+    
   }
-
-
 }
-
