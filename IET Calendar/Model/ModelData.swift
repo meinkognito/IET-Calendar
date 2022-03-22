33569eeeac37ac9562d361ab7d4ba366b9265a4b
Denis Beloshitskiy <beloshitskiy.denis@gmail.com>
2022-03-22 20:50:04 +0300

    Add .json to Dictionary decoder

--- IET Calendar/Model/ModelData.swift
+++ IET Calendar/Model/ModelData.swift
@@ -4,7 +4,7 @@ import UIKit
 let launchCount = UserDefaults.standard.integer(forKey: "launchCount")
 
 var guideTextBlocks: [GuideTextBlock] = load("guideTextBlocksData.json")
-var months: [Month] = load("monthsData.json")
+var months: [String: Month] = dictLoad("monthsData.json")
 
 func load<T: Decodable>(_ filename: String) -> T {
   let data: Data
@@ -25,5 +25,27 @@ func load<T: Decodable>(_ filename: String) -> T {
     return try decoder.decode(T.self, from: data)
   } catch {
     fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+  }
+}
+
+func dictLoad(_ filename: String) -> [String: Month] {
+  let data: Data
+
+  guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+  else {
+    fatalError("Couldn't find \(filename) in main bundle.")
+  }
+
+  do {
+    data = try Data(contentsOf: file)
+  } catch {
+    fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+  }
+
+  do {
+    let decoder = JSONDecoder()
+    return try decoder.decode([String: Month].self, from: data)
+  } catch {
+    fatalError("Couldn't parse \(filename) as \([String: Month].self):\n\(error)")
   }
 }
